7ac40224cff420d50e7640de4466dbbe53b068a2
Matheus Monteiro <mateusmonteiropereira2@gmail.com>
2023-11-14 07:18:03 -0300

    feat: component container modal and header

---
+++ src/components/containerModal/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledContainerModal = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 25px 15px;
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  /* .modalGuest {
+    width: 500px;
+    padding: 30px;
+    background-color: white;
+  } */
+`;
