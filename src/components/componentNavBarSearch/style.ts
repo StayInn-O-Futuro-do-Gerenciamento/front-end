45bec2a6f5eec80a46b66ede216958303f1759d5
maaxizin25 <maaxipb25@gmail.com>
2023-11-14 20:49:59 -0300

    feat/add-component-2

---
+++ src/components/componentNavBarSearch/style.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const NavBarSearchStyle = styled.div`
+  max-width: 1200px;
+  padding: 20px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  div:nth-child(1) {
+    display: flex;
+    align-items: center;
+    width: 300px;
+    background-color: var(--grey-50);
+    padding: 10px;
+    border-radius: 10px;
+    max-height: 40px;
+    gap: 10px;
+    input {
+      border: none;
+      width: 100%;
+      background-color: transparent;
+    }
+    input::placeholder {
+      border: none;
+    }
+    input:focus {
+      outline: none;
+    }
+  }
+  div:nth-child(2) {
+    display: flex;
+    gap: 20px;
+    img {
+      cursor: pointer;
+    }
+  }
+`;
