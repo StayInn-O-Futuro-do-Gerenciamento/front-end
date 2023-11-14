e29ceb681aaf2acf8c115a1011c4f7e81130df6c
maaxizin25 <maaxipb25@gmail.com>
2023-11-13 22:58:11 -0300

    create Provider

---
+++ src/context/appContext/type.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Typagem do children
+export interface iAppContextProps {
+  children: React.ReactNode;
+}
+// Typagem das funções/states que serão exportados.
+export interface iAppContext {
+  testState: boolean;
+  handleChangeFunction: (state: string, value: boolean) => void;
+}
