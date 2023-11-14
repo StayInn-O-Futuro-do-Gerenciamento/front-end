f588a61f5d8ab6c36d36ee21608ff3bf1855bcb3
Matheus Monteiro <mateusmonteiropereira2@gmail.com>
2023-11-13 23:48:22 -0300

    fix: file app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import { Form } from "./components/form";
-import { Input } from "./components/input";
-
 function App() {
-  return (
-    <>
-      <Form>
-        <Input type="number" label="Guest user" placeholder="Enterar" />
-        <Input type="number" label="Guest user" placeholder="Enterar" />
-        <Input type="number" label="Guest user" placeholder="Enterar" />
-        <Input type="number" label="Guest user" placeholder="Enterar" />
-        <span className="errorMessage">erooo de mais da conta</span>
-      </Form>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
