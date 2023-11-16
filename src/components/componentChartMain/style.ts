3c5ef7fb8d42a57ed461df776661fa6642e4777e
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-11-16 15:50:52 -0300

    fix: bar chart title

--- src/components/componentChartMain/style.ts
+++ src/components/componentChartMain/style.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const BarMain = styled.div`
   margin: 50px;
   width: 800px;
+  font-family: var(--font-family-inter);
+  h3 {
+    margin-bottom: 20px;
+  }
 `;
