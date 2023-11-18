e638bae8bb7588ff1d0b94d1bd2ec87b5a3f8973
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-11-17 21:59:46 -0300

    feat: table component, style global table

---
+++ src/style/tableStyle.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const TableStyled = styled.table`
+  font-family: var(--font-family-inter);
+  width: 100%;
+  background-color: gray;
+  border-collapse: separate;
+  border-spacing: 0;
+  border-radius: var(--border-radius);
+  border-right: 1px solid #e8f1fd;
+  border-left: 1px solid #e8f1fd;
+  overflow: hidden;
+
+  thead {
+    background-color: gray;
+    border: none;
+  }
+  tr {
+    border: none;
+    background-color: var(--grey-00);
+  }
+  th {
+    border: none;
+    background-color: #f7f9fc;
+    text-align: start;
+    padding: 15px;
+    font-weight: 500;
+    color: var(--grey-500);
+  }
+  td {
+    padding: 15px;
+    border-bottom: 1px solid var(--blue-50);
+    img {
+      cursor: pointer;
+    }
+  }
+
+  .firstNumber {
+    font-weight: var(--text-weight700);
+  }
+  .status {
+    border-radius: 16px;
+    color: var(--blue-600);
+    background-color: var(--blue-50);
+    width: max-content;
+    padding: 3px 8px 3px 8px;
+  }
+`;
